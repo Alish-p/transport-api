e9e9e7ff4d816b31388694d917a8e625e9b3064b
Alish Palasara <palasaraalis@gmail.com>
2024-12-05 00:34:37 +0530

    40d timer not supported by browsers

--- Utils/generateToken.js
+++ Utils/generateToken.js
@@ -1,4 +1,4 @@
 const jwt = require("jsonwebtoken");
 
 module.exports.generateToken = (id) =>
-  jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "40d" });
+  jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "4d" });
