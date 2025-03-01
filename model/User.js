034dfdd0a8053fd08cb5c3045d2977f89a01f13f
Alish Palasara <palasaraalis@gmail.com>
2025-03-01 15:42:02 +0530

    user permission feature

--- model/User.js
+++ model/User.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema(
     mobile: { type: String, required: true, unique: true },
     address: { type: String, required: true },
     password: { type: String, required: true },
+    designation: { type: String, required: true },
 
     permissions: {
       bank: {
