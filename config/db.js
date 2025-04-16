65ee06e7170d9918fbb1b97a0f4778c3d39e192d
Alish Palasara <palasaraalis@gmail.com>
2025-04-16 12:49:43 +0530

    connection pooling

--- config/db.js
+++ config/db.js
@@ -1,11 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI);
-    console.log('Database Connected !');
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      maxPoolSize: 10,
+    });
+    console.log("Database Connected !");
   } catch (error) {
-    console.log('Error Connecting to database');
+    console.log("Error Connecting to database");
     process.exit(1);
   }
 };
