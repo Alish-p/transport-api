550e581e671729813f1500f2687816b6023ecd0e
Alish Palasara <palasaraalis@gmail.com>
2025-06-16 13:45:44 +0530

    minor fixes

--- routes/transporter.js
+++ routes/transporter.js
@@ -8,14 +8,14 @@ const {
   fetchTransporterById,
 } = require("../controllers/transporter");
 
-const { private, admin, checkPermission } = require("../middlewares/Auth");
+const { private, checkPermission } = require("../middlewares/Auth");
 const pagination = require("../middlewares/pagination");
 
 const router = Router();
 
 router.post("/", private, checkPermission("transporter", "create"), createTransporter);
 router.get("/", private, fetchTransporters);
-router.get("/pagination", pagination, fetchPaginatedTransporters);
+router.get("/pagination", private, pagination, fetchPaginatedTransporters);
 router.get("/:id", private, fetchTransporterById);
 router.delete("/:id", private, checkPermission("transporter", "delete"), deleteTransporter);
 router.put("/:id", private, checkPermission("transporter", "update"), updateTransporter);
