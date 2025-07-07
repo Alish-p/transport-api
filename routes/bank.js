1162e69bca629c9d57e3731d7eda165941009e92
Alish-p <65279709+Alish-p@users.noreply.github.com>
2025-07-07 20:58:26 +0530

    Add pagination and search to fetchBanks

--- routes/bank.js
+++ routes/bank.js
@@ -8,10 +8,11 @@ const {
 } = require("../controllers/bank");
 
 const { private, admin, checkPermission } = require("../middlewares/Auth");
+const pagination = require("../middlewares/pagination");
 const router = Router();
 
 router.post("/", private, checkPermission("bank", "create"), createBank);
-router.get("/", private, fetchBanks);
+router.get("/", private, pagination, fetchBanks);
 router.get("/:id", private, fetchBankDetails);
 router.delete("/:id", private, checkPermission("bank", "delete"), deleteBank);
 router.put("/:id", private, checkPermission("bank", "update"), updateBank);
