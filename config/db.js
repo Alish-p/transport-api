a3ce843b7ea803cbfab6ccc44a363fc73c882742
Alish Palasara <palasaraalis@gmail.com>
2025-08-11 14:22:16 +0530

    minor fixes

--- config/db.js
+++ config/db.js
@@ -3,8 +3,6 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       maxPoolSize: 10,
     });
     console.log("Database Connected !");
