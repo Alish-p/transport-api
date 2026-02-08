eb1a623dbf69242effb78cac7322e18f65c8bc8d
Alish Palasara <palasaraalis@gmail.com>
2026-02-08 06:44:52 +0530

    dummy commit

--- entities/tyre/tyre.routes.js
+++ entities/tyre/tyre.routes.js
@@ -1,36 +1,37 @@
-import express from 'express';
-import { createTyre, getTyres, getTyreById, updateTyre, updateThreadDepth, mountTyre, unmountTyre, getTyreHistory, scrapTyre, updateTyreHistory } from './tyre.controller.js';
-import pagination from '../../middlewares/pagination.js';
-import { authenticate } from '../../middlewares/auth.js';
+import express from "express";
+import {
+    createTyre,
+    getTyres,
+    getTyreById,
+    updateTyre,
+    updateThreadDepth,
+    mountTyre,
+    unmountTyre,
+    getTyreHistory,
+    scrapTyre,
+    updateTyreHistory,
+} from "./tyre.controller.js";
+import pagination from "../../middlewares/pagination.js";
+import { authenticate } from "../../middlewares/auth.js";
 
 const router = express.Router();
 
 router.use(authenticate);
 
-router.route('/')
-    .post(createTyre)
-    .get(pagination, getTyres);
+router.route("/").post(createTyre).get(pagination, getTyres);
 
-router.route('/:id')
-    .get(getTyreById)
-    .put(updateTyre);
+router.route("/:id").get(getTyreById).put(updateTyre);
 
-router.route('/:id/thread')
-    .post(updateThreadDepth);
+router.route("/:id/thread").post(updateThreadDepth);
 
-router.route('/:id/mount')
-    .post(mountTyre);
+router.route("/:id/mount").post(mountTyre);
 
-router.route('/:id/unmount')
-    .post(unmountTyre);
+router.route("/:id/unmount").post(unmountTyre);
 
-router.route('/:id/history')
-    .get(getTyreHistory);
+router.route("/:id/history").get(getTyreHistory);
 
-router.route('/:id/history/:historyId')
-    .put(updateTyreHistory);
+router.route("/:id/history/:historyId").put(updateTyreHistory);
 
-router.route('/:id/scrap')
-    .post(scrapTyre);
+router.route("/:id/scrap").post(scrapTyre);
 
 export default router;
