509c20995ad38614ef3ae8fa967a758fa2fa67be
Alish Palasara <palasaraalis@gmail.com>
2026-02-11 00:09:59 +0530

    feat: add `type` field to driver model with 'Own' and 'Market' enum options

--- entities/driver/driver.model.js
+++ entities/driver/driver.model.js
@@ -18,6 +18,11 @@ const driverSchema = new Schema({
   photoImage: { type: String },
   aadharImage: { type: String },
   isActive: { type: Boolean, default: true },
+  type: {
+    type: String,
+    enum: ['Own', 'Market'],
+    set: (v) => (v === '' ? undefined : v),
+  },
   // Mark if license is past its validity
   expired: { type: Boolean, default: false },
   bankDetails: {
